e9dde0fedde6581e66d39aa22273ea1d54093e21
impruv <artyom2003gh@mail.ru>
2025-04-22 12:11:05 +0000

    добавлена динамическая подстановка временных этапов

--- js/script.js
+++ js/script.js
@@ -46,6 +46,41 @@ function initFilterHandler()
     });
 }
 
+function appendOptionsToSelect(filterElement, options)
+{
+    options.forEach(option => {
+        const listItem = document.createElement('option');
+        listItem.value = option[0];
+        listItem.text = option[1];
+        listItem.classList.add(...option[2]);
+
+        filterElement.appendChild(listItem);
+    });
+}
+
+function initFilterOptions()
+{
+    appendOptionsToSelect(document.getElementById('world-stages'), [
+        ['all', 'Все', ['main__world-stages-option', 'text_black']],
+        ['primitive-socie', 'Первобытное общество', ['main__world-stages-option', 'text_black']],
+        ['ancient-world', 'Древний мир', ['main__world-stages-option', 'text_black']],
+        ['middle-age', 'Средние века', ['main__world-stages-option', 'text_black']],
+        ['new-age', 'Новое время', ['main__world-stages-option', 'text_black']],
+        ['modern-age', 'Новейшее время', ['main__world-stages-option', 'text_black']],
+    ]);
+
+    appendOptionsToSelect(document.getElementById('russian-stages'), [
+        ['all', 'Все', ['main__world-stages-option', 'text_black']],
+        ['ancient-russian-state', 'Древнерусское государство', ['main__world-stages-option', 'text_black']],
+        ['feudal-fragmentation', 'Период феодальной раздробленности', ['main__world-stages-option', 'text_black']],
+        ['centralized-state', 'Образование централизированного русккого государства', ['main__world-stages-option', 'text_black']],
+        ['russian-empire', 'Российская империя', ['main__world-stages-option', 'text_black']],
+        ['soviet-union', 'Советсвикй период', ['main__world-stages-option', 'text_black']],
+        ['russian-federation', 'Российская федерация', ['main__world-stages-option', 'text_black']],
+    ]);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     initFilterHandler();
+    initFilterOptions();
 });
